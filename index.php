ef663f174b01d13f5c7217044ec66b26ea7e1a31
Jefferson Lucero <jeffersontlucero1009@gmail.com>
2025-11-20 02:07:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="0; url=Portfolio.php">
+    <title>Jefferson Lucero - Personal Portfolio</title>
+</head>
+
+<body>
+    <script>
+        window.location.href = "Portfolio.php";
+    </script>
+</body>
+
+</html>
